7ffc69dc35cb8651858a318ea652b13684296f46
Awexxus <samoshkinface@rambler.ru>
2018-09-01 11:58:42 +0300

    test1

--- test/js/main.js
+++ test/js/main.js
@@ -4,13 +4,15 @@ $(document).ready(function () {
         $('.menu_slider').animate({
             right: '0',
             opacity: '1',
-        },300)
+        },300);
+        $('.menu_icon_open').fadeOut(300);
     });
     $('.menu_icon_close>i').click(function(){
         $('.menu_slider').animate({
             right: '-285px',
             opacity: '0',
-        },300)
+        },300);
+        $('.menu_icon_open').fadeIn(300);
     });
     $(window).scroll(function () {
         if($(window).scrollTop()> 550){
@@ -29,15 +31,15 @@ $(document).ready(function () {
             });
         }
     });
-    $('.item_our').hover(function () {
-        $(this).find('h1').fadeIn(300);
-        $(this).find('p').fadeIn(300);
-        $(this).find('a').fadeIn(300);
-        },function () {
-        $(this).find('h1').fadeOut(300);
-        $(this).find('p').fadeOut(300);
-        $(this).find('a').fadeOut(300);
-    });
+        $('.item_our').hover(function () {
+            $(this).find('h1').fadeIn(300);
+            $(this).find('p').fadeIn(300);
+            $(this).find('a').fadeIn(300);
+        }, function () {
+            $(this).find('h1').fadeOut(300);
+            $(this).find('p').fadeOut(300);
+            $(this).find('a').fadeOut(300);
+        });
 
     $('.img').click(function () {
         $('.img').attr('src', './image/Cork12.png');
