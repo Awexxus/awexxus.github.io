3d66a1e4fc0670b06e78e04b04ffc9e2998f3f78
Awexxus <samoshkinface@rambler.ru>
2018-09-03 20:24:19 +0300

    test3/

--- test3/js/main.js
+++ test3/js/main.js
@@ -11,5 +11,34 @@ $(document).ready(function () {
     $('.prev_team').click(function () {
         prevSlide_p();
     });
+    console.log($(window).scrollTop())
+    $(window).scroll(function () {
+        if(($(window).scrollTop()) > 500 && $(window).width()> 768){
+            $('.design .des_txt').css({display: 'block'});
+            $('.design .des_txt').animate({
+                left: '0',
+            }, 600);
+        }
+        if(($(window).scrollTop()) > 1200 && $(window).width()> 768){
+            $('.one').animate({
+                padding: '0',
+            }, 400);
+            $('.two').animate({
+                padding: '0',
+            }, 500);
+            $('.three').animate({
+                padding: '0',
+            }, 600);
+        }
+        if(($(window).scrollTop())> 1800 && $(window).width()> 768){
+            $('.resp_image').fadeIn(600)
+        }
+        if(($(window).scrollTop())> 3100 && $(window).width()> 768){
+            $('.awes_text img').animate({
+                margin: '0'
+            }, 500)
+        }
+    })
+
 
 })
