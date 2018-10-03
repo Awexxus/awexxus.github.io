547dca9a5a9ebf0b9cf16827f6286a6b0fbd9715
Awexxus <samoshkinface@rambler.ru>
2018-10-03 11:41:16 +0300

    v1

--- crud-2.0/src/apimethods/getItems.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-    return fetch('http://server.noorsoft.ru:9022/api/records')
-        .then(res => {return res.json()})
-        .then(function(result){
-            return result
-        })
-        .catch(error => console.log(error))
-}
