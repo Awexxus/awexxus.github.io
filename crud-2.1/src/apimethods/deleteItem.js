3e4cdeba332c2e8de9b6c496c1c8d9b777974c91
Awexxus <samoshkinface@rambler.ru>
2018-10-03 11:44:01 +0300

    test

--- crud-2.1/src/apimethods/deleteItem.js
+++
@@ -1,7 +0,0 @@
-export default (id) => {
-    return fetch('http://server.noorsoft.ru:9022/api/records/'+id, {
-        method: 'DELETE',
-    }).then(res =>{
-        return res
-    }).catch(error => console.log(error))
-}
